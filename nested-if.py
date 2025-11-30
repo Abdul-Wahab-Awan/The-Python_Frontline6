efc5f32d2e53d9ebe456321ad83e50987e97d8d8
Abdul Wahab Awan <167404782+Abdul-Wahab-Awan@users.noreply.github.com>
2025-12-01 00:23:25 +0500

    Add files via upload

---
+++ nested-if.py
@@ -0,0 +1,9 @@
+number=int(input("Enter any Num: "))
+if number>0:
+    print("Number Positive hai");
+    if number%2==0:
+        print("Number is Even ");
+    else:
+        print("Number is Odd");
+else:
+    print("Number is not positve ");
